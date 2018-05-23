a13838e195be23db30a50f250743b2db36657fa2
CHENG KAI JUNG <f45646ssdg1@gmail.com>
2018-05-23 20:57:24 +0800

    test2

--- src/DoubleSixTest.java
+++ src/DoubleSixTest.java
@@ -2,9 +2,9 @@
 public class DoubleSixTest extends DoubleSix{
 
 	public static void main(String[] args) {
-		DoubleSix six = new DoubleSix();
-		six.setSize(800, 600);
-		six.setVisible(true);
+		DoubleSix dou = new DoubleSix();
+		dou.setSize(1300, 900);
+		dou.setVisible(true);
 
 	}
 
