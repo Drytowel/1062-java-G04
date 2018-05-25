02fee3049a57db92ea38d644ab573fe1d8ddc528
Chao-Eric <39295333+Chao-Eric@users.noreply.github.com>
2018-05-26 01:52:36 +0800

    Add files via upload
    action listener(not yet)

--- src/DoubleSix.java
+++ src/DoubleSix.java
@@ -13,9 +13,12 @@ import java.awt.Color;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 
-public class DoubleSix extends JFrame {
+public class DoubleSix extends JFrame implements ActionListener {
 
 	private JPanel contentPane;
+	private JButton btnNewButton;
+	private JButton btnNewButton_1;
+	private JLabel testLB;
 
 	/**
 	 * Launch the application.
@@ -28,7 +31,7 @@ public class DoubleSix extends JFrame {
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
-				}
+				} //catch the exception situation
 			}
 		});
 	}
@@ -37,6 +40,7 @@ public class DoubleSix extends JFrame {
 	 * Create the frame.
 	 */
 	public DoubleSix() {
+		super();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 655, 534);
 		contentPane = new JPanel();
@@ -48,22 +52,32 @@ public class DoubleSix extends JFrame {
 		JButton btnNewButton = new JButton("\u904A\u6232\u958B\u59CB");
 		btnNewButton.setBounds(176, 369, 291, 31);
 		btnNewButton.setBackground(Color.WHITE);
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-			}
-		});
+		btnNewButton.addActionListener(this);
+		
 		contentPane.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("\u898F\u5247");
 		btnNewButton_1.setBounds(176, 312, 291, 31);
+		btnNewButton_1.addActionListener(this);
 		contentPane.add(btnNewButton_1);
 		
 		JLabel testLB = new JLabel("");
-		testLB.setIcon(new ImageIcon("C:\\Users\\user\\Pictures\\\u4E0B\u8F09.png"));
+		testLB.setIcon(new ImageIcon("C:/Users/Administrator/Desktop/JavaProgram/temp_final_project/Double_six/src/123.png"));
 		testLB.setBounds(63, 41, 225, 181);
 		contentPane.add(testLB);
 		
+		
+	}
+
+	//listening the actions
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==btnNewButton) {
+			System.out.println("this is btnNewButton");
+		}else if(e.getSource()==btnNewButton_1) {
+			System.out.println("this is btnNewButton_1");
+		}
 		
 	}
 }
