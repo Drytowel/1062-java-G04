2e7e37a8bfe94f7f7ab0e44f2364d72e1fb988e3
1062-java-G04 <39294949+Drytowel@users.noreply.github.com>
2018-05-26 23:17:36 +0800

    指定src內的圖片

--- src/DoubleSix.java
+++ src/DoubleSix.java
@@ -11,6 +11,7 @@ import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
 import java.awt.Color;
 import javax.swing.JLabel;
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
 public class DoubleSix extends JFrame implements ActionListener {
@@ -55,7 +56,8 @@ public class DoubleSix extends JFrame implements ActionListener {
 		contentPane.add(btnNewButton_1);
 		
 		testLB = new JLabel("");
-		testLB.setIcon(new ImageIcon("C:/Users/Administrator/Desktop/JavaProgram/temp_final_project/Double_six/src/123.png"));
+		testLB.setIcon(new ImageIcon(getClass().getResource( "123.png" ) ));
+		//Icon bug = new ImageIcon( getClass().getResource( "bug1.png" ) );
 		testLB.setBounds(63, 41, 225, 181);
 		contentPane.add(testLB);
 		
