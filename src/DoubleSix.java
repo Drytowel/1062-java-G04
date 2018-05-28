60df88eda2470bdd08e56d66f95ff5dd48a69593
lxlx9501 <lxlx9501@yahoo.com.tw>
2018-05-28 23:10:03 +0800

    name

--- src/DoubleSix.java
+++ src/DoubleSix.java
@@ -18,13 +18,12 @@ import javax.swing.ImageIcon;
 
 public class DoubleSix extends JFrame implements ActionListener {
 
-	private DoubleSixPanel DoubleSixPanel;
-	private JPanel contentPane;
-	private JPanel contentPane_2;
-	private JButton btnNewButton;      //戰鬥開始
-	private JButton btnNewButton_1;    //規則
-	private JButton btnNewButton_2;    //返回
-	private JTextArea btnNewButton_3;    //規則內容	
+	private DoubleSixPanel doubleSixPanel;
+	private JPanel contentPanel;
+	private JButton gameStart;
+	private JButton rule; 
+	private JButton back;
+	private JTextArea ruleContent;
 	private JLabel testLB;
 
 	/**
@@ -38,34 +37,34 @@ public class DoubleSix extends JFrame implements ActionListener {
 	public DoubleSix() {
 		super();
 		
-		DoubleSixPanel = new DoubleSixPanel();
+		doubleSixPanel = new DoubleSixPanel();
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//setBounds(100, 100, 655, 534);
-		contentPane = new JPanel();
+		contentPanel = new JPanel();
 		//contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		setContentPane(contentPanel);
+		contentPanel.setLayout(null);
 		
 		
-		btnNewButton = new JButton("\u904A\u6232\u958B\u59CB");
-		btnNewButton.setBounds(350, 369, 500, 31);
+		gameStart = new JButton("\u904A\u6232\u958B\u59CB");
+		gameStart.setBounds(350, 369, 500, 31);
 		//btnNewButton.setBackground(Color.WHITE);
-		btnNewButton.addActionListener(this);
+		gameStart.addActionListener(this);
 		
-		contentPane.add(btnNewButton);
+		contentPanel.add(gameStart);
 		
-		btnNewButton_1 = new JButton("\u898F\u5247");
-		btnNewButton_1.setBounds(350, 312, 500, 31);
-		btnNewButton_1.addActionListener(this);
+		rule = new JButton("\u898F\u5247");
+		rule.setBounds(350, 312, 500, 31);
+		rule.addActionListener(this);
 		
-		contentPane.add(btnNewButton_1);
+		contentPanel.add(rule);
 		
 		testLB = new JLabel("");
 		testLB.setIcon(new ImageIcon(getClass().getResource( "wood.png" ) ));
 		//Icon bug = new ImageIcon( getClass().getResource( "bug1.png" ) );
 		testLB.setBounds(0, 0, 3000, 500);
-		contentPane.add(testLB);
+		contentPanel.add(testLB);
 		
 		
 	}
@@ -74,20 +73,20 @@ public class DoubleSix extends JFrame implements ActionListener {
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource()==btnNewButton) {
-			System.out.println("this is btnNewButton");
-		}else if(e.getSource()==btnNewButton_1) {
-			System.out.println("this is btnNewButton_1");
+		if(e.getSource()==gameStart) {
+			System.out.println("this is gameStart");
+		}else if(e.getSource()==rule) {
+			System.out.println("rule");
 			setSize(1300, 524);
 			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			//setBounds(100, 100, 655, 534);
-			contentPane_2 = new JPanel();
+			contentPanel = new JPanel();
 			//contentPane_2.setBorder(new EmptyBorder(5, 5, 5, 5));
-			setContentPane(contentPane_2);
-			contentPane_2.setLayout(new GridBagLayout());		
+			setContentPane(contentPanel);
+			contentPanel.setLayout(new GridBagLayout());		
 			
 			
-			JTextArea btnNewButton_3 = new JTextArea("遊戲名稱:雙六\r\n" + 
+			JTextArea ruleContent = new JTextArea("遊戲名稱:雙六\r\n" + 
 					" \r\n" + 
 					"遊戲方式  : 每個玩家丟一個骰子(以 _自選顏色_ 當玩家代表)，毎個格子都有特殊的小規則，可能是前進幾格或後退幾格，或者是要完成某個題目。  \r\n" + 
 					"\r\n" + 
@@ -104,45 +103,45 @@ public class DoubleSix extends JFrame implements ActionListener {
 					"   前進後退  : 踩到就前進或後退該格規定之格數  \r\n" + 
 					"  \r\n" + 
 					"   特殊格     : 踩到就回家、踩到就進監獄  ");			
-			btnNewButton_3.setFont (new Font ("SansSerif", Font.PLAIN, 18));  //字體大小
-			contentPane_2.add(btnNewButton_3);
-			btnNewButton_2 = new JButton("返回");
-			btnNewButton_2.setBounds(0, 0, 500, 31);
-			btnNewButton_2.addActionListener(this);
-			contentPane_2.add(btnNewButton_2);
+			ruleContent.setFont (new Font ("SansSerif", Font.PLAIN, 18));  //字體大小
+			contentPanel.add(ruleContent);
+			back = new JButton("返回");
+			back.setBounds(0, 0, 500, 31);
+			back.addActionListener(this);
+			contentPanel.add(back);
 			setVisible(true);
-		}else if(e.getSource()==btnNewButton_2) {                              
+		}else if(e.getSource()==back) {                              
 			System.out.println("this is btnNewButton_3");
 			
-			DoubleSixPanel = new DoubleSixPanel();                 
+			doubleSixPanel = new DoubleSixPanel();                 
 			DoubleSix dou = new DoubleSix();
 			//dou.setSize(1300, 525);
 			
 			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			setBounds(100, 100, 1300, 525);
-			contentPane = new JPanel();
+			contentPanel = new JPanel();
 			//contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-			setContentPane(contentPane);
-			contentPane.setLayout(null);
+			setContentPane(contentPanel);
+			contentPanel.setLayout(null);
 			
 			
-			btnNewButton = new JButton("\u904A\u6232\u958B\u59CB");
-			btnNewButton.setBounds(350, 369, 500, 31);
-			btnNewButton.setBackground(Color.WHITE);
-			btnNewButton.addActionListener(this);
+			gameStart = new JButton("\u904A\u6232\u958B\u59CB");
+			gameStart.setBounds(350, 369, 500, 31);
+			gameStart.setBackground(Color.WHITE);
+			gameStart.addActionListener(this);
 			
-			contentPane.add(btnNewButton);
+			contentPanel.add(gameStart);
 			
-			btnNewButton_1 = new JButton("\u898F\u5247");
-			btnNewButton_1.setBounds(350, 312, 500, 31);
-			btnNewButton_1.addActionListener(this);
-			contentPane.add(btnNewButton_1);
+			rule = new JButton("\u898F\u5247");
+			rule.setBounds(350, 312, 500, 31);
+			rule.addActionListener(this);
+			contentPanel.add(rule);
 			
 			testLB = new JLabel("");
 			testLB.setIcon(new ImageIcon(getClass().getResource( "wood.png" ) ));
 			//Icon bug = new ImageIcon( getClass().getResource( "bug1.png" ) );
 			testLB.setBounds(0, 0, 3000, 500);
-			contentPane.add(testLB);
+			contentPanel.add(testLB);
 			
 		}
 		
