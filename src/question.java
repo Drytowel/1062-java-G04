f628662a434808b6c8a96742771e42b5cdcb7b7c
1062-java-G04 <39294949+Drytowel@users.noreply.github.com>
2018-06-04 21:23:01 +0800

    更改字體大小

--- src/question.java
+++ src/question.java
@@ -1,8 +1,10 @@
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
+import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -14,6 +16,8 @@ public class question extends JFrame{
 	private JPanel botp = new JPanel();
 	private GridBagLayout gb = new GridBagLayout();
 	private GridBagConstraints gbc = new GridBagConstraints();
+	private Box horizontal = Box.createHorizontalBox();
+	private Box vertical = Box.createVerticalBox();
 	private JButton q1 = new JButton("鯛魚燒");
 	private JButton q2 = new JButton("銅鑼燒 ");
 	private JButton q3 = new JButton("豬血糕");
@@ -21,28 +25,33 @@ public class question extends JFrame{
 	
 	public void init() {
 		qf.setBounds(30, 30, 500, 300);
+		
+//		vertical.add(Box.createVerticalStrut(10));
+//		vertical.add(q1);
+//		vertical.add(Box.createVerticalStrut(10));
+//		vertical.add(q2);
+//		vertical.add(Box.createVerticalStrut(10));
+//		vertical.add(q3);
+//		vertical.add(Box.createVerticalGlue());
+//		vertical.add(q4);
 		qf.setLayout(new BorderLayout(0, 20));
-//		botp.setLayout(gb);
+//		
 		botp.setLayout(new FlowLayout(FlowLayout.CENTER, 200, 15));
-		
+//		
 		JTextArea text = new JTextArea("問題: 哆啦A夢最喜愛的食物是？ ");
+		text.setFont (new Font ("SansSerif", Font.PLAIN, 18));
+		
 		topp.add(text);
-		
-//		gbc.fill = GridBagConstraints.HORIZONTAL;
-//		gb.setConstraints(q1, gbc);
+//	
 		botp.add(q1);
-		
-//		gbc.gridwidth = 5;
-////		gbc.gridwidth = GridBagConstraints.REMAINDER;
 		botp.add(q2);
-//		gbc.gridwidth = GridBagConstraints.REMAINDER;
 		botp.add(q3);
-//		gbc.gridwidth = GridBagConstraints.REMAINDER;
 		botp.add(q4);
-//		gbc.gridwidth = GridBagConstraints.REMAINDER;
-		
+//		
 		qf.add(topp, BorderLayout.NORTH);
 		qf.add(botp);
+//		qf.add(vertical, BorderLayout.CENTER);
+//		qf.pack();
 		qf.setVisible(true);
 		
 	}
