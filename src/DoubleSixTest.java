8095dbb52f24f6799653b660cfe25075d6c9642b
1062-java-G04 <39294949+Drytowel@users.noreply.github.com>
2018-06-04 20:40:49 +0800

    使視窗無法改變大小、設置視窗名稱

--- src/DoubleSixTest.java
+++ src/DoubleSixTest.java
@@ -4,6 +4,8 @@ public class DoubleSixTest extends DoubleSix{
 	public static void main(String[] args) {
 		DoubleSix dou = new DoubleSix();
 		dou.setSize(1300, 525);
+		dou.setResizable(false);
+		dou.setTitle("Âù¤»");
 		dou.setVisible(true);
 
 	}
