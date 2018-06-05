d1efeab84422d64d3a227db1ae829f3e25e6937c
1062-java-G04 <39294949+Drytowel@users.noreply.github.com>
2018-06-05 22:43:54 +0800

    UTF8?

--- src/DoubleSix.java
+++ src/DoubleSix.java
@@ -27,12 +27,12 @@ import javax.swing.ImageIcon;
 
 public class DoubleSix extends JFrame implements ActionListener {
 
-	private JPanel startPanel;               //����anel
+	private JPanel startPanel;               //開始的Panel
 	private JButton gameStart;	
 	private JButton rule; 
 	private JLabel backgroundLB;
 	
-	private JPanel rulePanel;                //rule��anel
+	private JPanel rulePanel;                 //rule的Panel
 	private JButton back;	
 	
 	private JPanel playPanel;                
@@ -40,8 +40,8 @@ public class DoubleSix extends JFrame implements ActionListener {
 	private JButton backToMenu;
 	
 	
-	private final static int ROW = 10; //璈怠��10�摮�
-	private final static int COLUMN = 5; //蝮勗��5�摮�
+	private final static int ROW = 10;  //橫向10個格子
+	private final static int COLUMN = 5; //縱向5個格子
 	
 	private JButton btn[];
 	private Question question;
@@ -91,9 +91,9 @@ public class DoubleSix extends JFrame implements ActionListener {
 		if(e.getSource()==gameStart) {
 			System.out.println("gameStart");
 			
-			GridLayout gridLayout = new GridLayout(ROW, COLUMN); //閮剔蔭�摮�Panel
+			GridLayout gridLayout = new GridLayout(ROW, COLUMN); //設置格子狀Panel
 			
-			Border border = BorderFactory.createBevelBorder(BevelBorder.LOWERED, //閮剔蔭����
+			Border border = BorderFactory.createBevelBorder(BevelBorder.LOWERED, //設置邊框
 			new Color(45, 92, 162), 
 			new Color(43, 66, 97), 
 			new Color(45, 92, 162), 
@@ -103,38 +103,38 @@ public class DoubleSix extends JFrame implements ActionListener {
 			playPanel = new JPanel();
 			playPanel.setBounds(100, 100, 1300, 524);
 			playPanel.setLayout(new BorderLayout());
-			setContentPane(playPanel);		//閮剔蔭�JFrame獢��摰寥�
+			setContentPane(playPanel);		//設置為JFrame框架的內容面板
 			
 			
-			about = new JButton("��"); //閮剔蔭������
+			about = new JButton("關於"); //設置關於按鈕
 			about.addActionListener(this);
 			
-			backToMenu = new JButton("��銝餌�"); //閮剔蔭��銝餌�����
+			backToMenu = new JButton("回到主畫面"); //設置回到主畫面按鈕
 			backToMenu.addActionListener(this);
 
 			
-			JPanel toolBar = new JPanel();   //閮剔蔭撌亙���
+			JPanel toolBar = new JPanel();   //設置工具列
 			toolBar.setBackground(Color.white);
-			toolBar.setBorder(border); //閮剔蔭����
+			toolBar.setBorder(border); //設置邊框
 			toolBar.setPreferredSize(new Dimension(1000, 48));
 			
-			toolBar.add(about); //撠�����
-			toolBar.add(backToMenu); //��銝餌�����
+			toolBar.add(about); //將關於按鈕放入
+			toolBar.add(backToMenu); //回到主畫面按鈕
 			
-			JPanel actionPanel = new JPanel();  //閮剔蔭�������
+			JPanel actionPanel = new JPanel();  //設置操作區域
 			actionPanel.setBackground(Color.yellow);
 			actionPanel.setBorder(border);
 			actionPanel.setPreferredSize(new Dimension(520, 380));
 			
-			JLabel player = new JLabel("��",JLabel.CENTER);
-			player.setFont (new Font ("SansSerif", Font.PLAIN, 40));
-			actionPanel.add(player);
+			JLabel score = new JLabel("分數",JLabel.CENTER);        //設置分數在分數面板
+			score.setFont (new Font ("SansSerif", Font.PLAIN, 40));
+			actionPanel.add(score);
 			
-			JPanel contentPanel = new JPanel();  //閮剔蔭�������
+			JPanel contentPanel = new JPanel();  //設置遊玩區域
 			contentPanel.setBackground(Color.blue);
 			contentPanel.setBorder(border);
 			contentPanel.setPreferredSize(new Dimension(300, 500));
-			contentPanel.setLayout(gridLayout); //撠�����身��ridLayout
+			contentPanel.setLayout(gridLayout); //將遊戲區域設成gridLayout
 			
 			btn = new JButton[51];
 			for(int i=1; i<=ROW*COLUMN; i++) {								
@@ -149,72 +149,7 @@ public class DoubleSix extends JFrame implements ActionListener {
 			playPanel.add(toolBar, BorderLayout.NORTH);
 			playPanel.add(actionPanel, BorderLayout.EAST);
 			playPanel.add(contentPanel, BorderLayout.CENTER);
-			
-
-			
-//			JButton n1 = new JButton("1");
-//	        GridBagConstraints p1 = new GridBagConstraints();
-//	        p1.gridx = 0;
-//	        p1.gridy = 0;
-//	        p1.gridwidth = 1;
-//	        p1.gridheight = 1;
-//	        p1.weightx = 0;
-//	        p1.weighty = 0;
-//	        p1.fill = GridBagConstraints.BOTH;
-//	        p1.anchor = GridBagConstraints.NORTHEAST;
-//	        playPanel.add(n1, p1);
-//	        
-//	        
-//	        
-//	        JButton n2 = new JButton("2");
-//	        GridBagConstraints p2 = new GridBagConstraints();
-//	        p2.gridx = 1;
-//	        p2.gridy = 0;
-//	        p2.gridwidth = 1;
-//	        p2.gridheight = 1;
-//	        p2.weightx = 0;
-//	        p2.weighty = 0;
-//	        p2.fill = GridBagConstraints.NONE;
-//	        p2.anchor = GridBagConstraints.NORTHEAST;	        
-//	        playPanel.add(n2, p2);
-//	        
-//	        JButton n3 = new JButton("3");	        
-//	        GridBagConstraints p3 = new GridBagConstraints();
-//	        p3.gridx = 2;
-//	        p3.gridy = 0;
-//	        p3.gridwidth = 1;
-//	        p3.gridheight = 1;
-//	        p3.weightx = 0;
-//	        p3.weighty = 0;
-//	        p3.fill = GridBagConstraints.NONE;
-//	        p3.anchor = GridBagConstraints.NORTHEAST;
-//	        playPanel.add(n3, p3);
-//	  
-//	        JButton n4 = new JButton("4");	        
-//	        GridBagConstraints p4 = new GridBagConstraints();
-//	        p4.gridx = 3;
-//	        p4.gridy = 0;
-//	        p4.gridwidth = 1;
-//	        p4.gridheight = 1;
-//	        p4.weightx = 0;
-//	        p4.weighty = 0;
-//	        p4.fill = GridBagConstraints.NONE;
-//	        p4.anchor = GridBagConstraints.NORTHEAST;
-//	        playPanel.add(n4, p4);
-//			
-//	        
-//	        JButton n5 = new JButton("5");	        
-//	        GridBagConstraints p5 = new GridBagConstraints();
-//	        p5.gridx = 43;
-//	        p5.gridy = 0;
-//	        p5.gridwidth = 1;
-//	        p5.gridheight = 1;
-//	        p5.weightx = 0;
-//	        p5.weighty = 0;
-//	        p5.fill = GridBagConstraints.NONE;
-//	        p5.anchor = GridBagConstraints.NORTHEAST;
-//	        playPanel.add(n5, p5);
-	        
+		
 	        
 	        playPanel.setVisible(true);
 			
@@ -225,28 +160,28 @@ public class DoubleSix extends JFrame implements ActionListener {
 			setContentPane(rulePanel);
 //          contentPane_2.setBorder(new EmptyBorder(5, 5, 5, 5));			
 			rulePanel.setLayout(new GridBagLayout());				
-			JTextArea ruleContent = new JTextArea("����迂:��\r\n" + 
-					" \r\n" + 
-					"���撘�  : 瘥�摰嗡���狐摮�(隞� _��憿_ ��摰嗡誨銵�)嚗��摮��畾����������脣嗾������撟暹嚗��閬�������  \r\n" + 
-					"\r\n" + 
-					"�摰嗡犖�  : 2\r\n" + 
-					"\r\n" + 
-					"��璇辣  : '�����' ������犖����  \r\n" + 
-					"\r\n" + 
-					"蝮賣� : 50�  \r\n" + 
-					"\r\n" + 
-					"撠��車憿� :  \r\n" + 
-					"\r\n" + 
-					"   ���� : �����,�霅��,敺桃���,撣貉���  \r\n" + 
-					"  \r\n" + 
-					"   ���脣��� : 頦拙撠勗��脫����閰脫閬���  \r\n" + 
-					"  \r\n" + 
-					"   �畾 : 頦拙撠勗�振�萱�撠梢�脩���  ");			
-			ruleContent.setFont (new Font ("SansSerif", Font.PLAIN, 18));  //摮�之撠�
-			ruleContent.setBackground (Color.white);                       //��憿
-			ruleContent.setForeground (Color.BLACK);                       //摮��
+			JTextArea ruleContent = new JTextArea("遊戲名稱:雙六\r\n" + 
+ 					" \r\n" + 
+ 					"遊戲方式  : 每個玩家丟一個骰子(以 _自選顏色_ 當玩家代表)，毎個格子都有特殊的小規則，可能是前進幾格或後退幾格，或者是要完成某個題目。  \r\n" + 
+ 					"\r\n" + 
+ 					"玩家人數  : 2\r\n" + 
+ 					"\r\n" + 
+ 					"勝利條件  : '最先' 到達終點的人勝利。  \r\n" + 
+ 					"\r\n" + 
+ 					"總格數 : 50格  \r\n" + 
+ 					"\r\n" + 
+ 					"小規則種類 :  \r\n" + 
+ 					"\r\n" + 
+ 					"   猜題 : 猜燈謎,知識王,微積分,常識題  \r\n" + 
+ 					"  \r\n" + 
+ 					"   前進後退 : 踩到就前進或後退該格規定之格數  \r\n" + 
+ 					"  \r\n" + 
+ 					"   特殊格 : 踩到就回家、踩到就進監獄  ");			
+			ruleContent.setFont (new Font ("SansSerif", Font.PLAIN, 18));  //字體大小
+			ruleContent.setBackground (Color.white);                       //背景顏色
+			ruleContent.setForeground (Color.BLACK);                       //字体颜色
 			rulePanel.add(ruleContent);			
-			back = new JButton("餈��");
+			back = new JButton("返回");
 			back.setBounds(0, 0, 500, 31);
 			back.addActionListener(this);
 			rulePanel.add(back);			
@@ -278,8 +213,8 @@ public class DoubleSix extends JFrame implements ActionListener {
 		}
 		else if(e.getSource()==about) {
 			System.out.print("about");
-			JOptionPane.showMessageDialog(this, "鋆賭��: G04  �璆瑁�����������誨",
-					"��", JOptionPane.INFORMATION_MESSAGE);
+			JOptionPane.showMessageDialog(this, "製作: G04  鄭楷融、鄧煒庠、何冠勳、趙崇廣",
+ 					"關於", JOptionPane.INFORMATION_MESSAGE);
 			return;
 		}
 		else if(e.getSource()==backToMenu) {
