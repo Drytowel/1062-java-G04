acc2d5d75b0e02c5c54d5a6c7d8e91ac04a07f16
1062-java-G04 <39294949+Drytowel@users.noreply.github.com>
2018-06-11 23:49:27 +0800

    重設結束畫面

--- src/Gameover.java
+++ src/Gameover.java
@@ -18,41 +18,27 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-public class Gameover extends JPanel implements ActionListener{
-//	private JPanel topPanel;
-	private JButton playAgain;
-	private JButton backToMenu;
+public class Gameover extends JPanel{
 	private String score;
 	private JLabel information;
 	private String input;
-	private JTextField scoreBoard;
 	private JPanel textPanel;
-	
-
 	
 	public Gameover() {
 		this.setBounds(0, 0, 1300, 525);
 		this.setLayout(new BorderLayout());
 		
-//		topPanel = new JPanel();
+		input = DoubleSix.getInput();
+		System.out.println(input);
 		
-//		playAgain = new JButton("在玩一次?");
-//		backToMenu = new JButton("返回主畫面");
-//		topPanel.add(playAgain);
-//		topPanel.add(backToMenu);
+		score = "     您的分數: " + DoubleSix.getSum();
+		information = new JLabel();
+		information.setBackground(Color.white);
+		information.setFont(new Font ("SansSerif", Font.BOLD, 40));
+		information.setForeground(Color.BLUE);
+		information.setText("您的姓名: "+ input + "\r\n" + score);
 		
-//		input = DoubleSix.getInput();
-//		System.out.println(input);
-//		
-//		score = "     您的分數: " + DoubleSix.getSum();
-//		information = new JLabel();
-//		information.setBackground(Color.white);
-//		information.setFont(new Font ("SansSerif", Font.BOLD, 40));
-//		information.setForeground(Color.BLUE);
-//		information.setText("您的姓名: "+ input + "\r\n" + score);
-//		
-//		this.add(information);
-//		this.add(topPanel, BorderLayout.NORTH);
+		this.add(information);
         
 		//抓資料
 		try {
@@ -82,21 +68,7 @@ public class Gameover extends JPanel implements ActionListener{
 				
 				
 			    for(int i=1; i<=sum; i++){
-//			    	textPanel = new JPanel();
-//			    	scoreBoard = new JTextField("                 ");
-//			    	scoreBoard.setText(data.getColumnName(i)+":"+rs.getObject(i)+"\t");
-//			    	scoreBoard.setFont(new Font ("SansSerif", Font.PLAIN, 20));
-//			    	textPanel.add(scoreBoard);
-//			    	this.add(textPanel);
-			    	
-			    	
-//			    	textPanel.setLayout(new GridBagLayout());
-//					JTextArea scoreBoard = new JTextArea(data.getColumnName(i+1)+":"+rs.getObject(i+1)+" \t");
-//					scoreBoard.setEditable(false); //使規則無法被更改
-//					scoreBoard.setFont (new Font ("SansSerif", Font.PLAIN, 30));  //字體大小
-//					scoreBoard.setBackground (Color.white);                       //背景顏色
-//					scoreBoard.setForeground (Color.BLACK);                       //字体颜色
-//					textPanel.add(scoreBoard);
+
 
 					this.add(textPanel);
 			    	
@@ -123,21 +95,6 @@ public class Gameover extends JPanel implements ActionListener{
 		this.input = input;
 	}
 
-
-
-
-
-
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if(e.getSource()==playAgain) {
-			
-		}
-		if(e.getSource()==backToMenu) {
-
-		}
-	}
 	
 
 }
